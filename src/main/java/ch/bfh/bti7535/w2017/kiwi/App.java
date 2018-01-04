b5b01d33591cf0395b69ca46d56cd3a34980b818
Jan Scheidegger <scheideggerj@gmail.com>
2018-01-04 13:50:42 +0100

    81.25% correctly classified

--- src/main/java/ch/bfh/bti7535/w2017/kiwi/App.java
+++ src/main/java/ch/bfh/bti7535/w2017/kiwi/App.java
@@ -1,19 +1,13 @@
 package ch.bfh.bti7535.w2017.kiwi;
 
 import ch.bfh.bti7535.w2017.kiwi.filter.Tokenizer;
-import ch.bfh.bti7535.w2017.kiwi.loader.AttributeLoader;
-import ch.bfh.bti7535.w2017.kiwi.loader.FolderLoader;
 import weka.classifiers.Evaluation;
-import weka.classifiers.bayes.NaiveBayesUpdateable;
+import weka.classifiers.bayes.NaiveBayes;
+import weka.classifiers.evaluation.output.prediction.HTML;
 import weka.core.Instances;
 import weka.core.converters.TextDirectoryLoader;
 
 import java.io.File;
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Random;
 
 /**
@@ -24,41 +18,30 @@ public class App {
     public static void main(String[] args) throws Exception {
         TextDirectoryLoader loader = new TextDirectoryLoader();
         loader.setDirectory(new File(App.class.getClassLoader()
-                                              .getResource("txt_sentoken").toURI()));
+                                              .getResource("txt_sentoken")
+                                              .toURI()));
         Instances dataSet = loader.getDataSet();
         System.out.println(dataSet.toSummaryString());
         System.out.println(dataSet);
         Tokenizer tokenizer = new Tokenizer(dataSet);
-        try {
-            Instances filteredInstances = tokenizer.apply();
-            System.out.println(filteredInstances.toSummaryString());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        Instances structure = loader.getStructure();
-        structure.setClassIndex(structure.numAttributes() - 1);
+        Instances filteredInstances = tokenizer.apply();
+        System.out.println(filteredInstances.toSummaryString());
+
+        NaiveBayes model = new NaiveBayes();
+
+        // Do not pre build classifier for 10 Fold Cross Validation
+        //model.buildClassifier(filteredInstances);
+
+        Evaluation eval = new Evaluation(filteredInstances);
+        HTML outputHtml = new HTML();
+        StringBuffer buffer = new StringBuffer();
+        outputHtml.setBuffer(buffer);
+
+        eval.crossValidateModel(model, filteredInstances, 10, new Random(1), outputHtml);
+
+        System.out.println(eval.toSummaryString());
+        System.out.println(eval.toClassDetailsString());
 
     }
 
-    public static void main2(String[] args) throws URISyntaxException {
-        // Load Data
-        URL resourceUrl = App.class.getClassLoader()
-                                   .getResource("txt_sentoken");
-        Path resourcePath = Paths.get(resourceUrl.toURI());
-        FolderLoader loader = new FolderLoader(resourcePath, "", new AttributeLoader[]{});
-        Instances instances = loader.getInstances();
-        System.out.println(instances.toSummaryString());
-
-
-        // Filter and Tokenize Data
-        Tokenizer tokenizer = new Tokenizer(instances);
-        try {
-            Instances filteredInstances = tokenizer.apply();
-            System.out.println(filteredInstances.toSummaryString());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-
-    }
 }
