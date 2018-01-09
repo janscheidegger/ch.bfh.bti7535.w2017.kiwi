e177e3aac56fea4c894c16e357b26a985edaa1eb
simu83 <simon.schaad@students.bfh.ch>
2018-01-09 19:35:52 +0100

    Weighted Baseline mit SentiWordNet

--- src/main/java/ch/bfh/bti7535/w2017/kiwi/App.java
+++ src/main/java/ch/bfh/bti7535/w2017/kiwi/App.java
@@ -4,6 +4,7 @@ import ch.bfh.bti7535.w2017.kiwi.filter.NGramRainbow;
 import ch.bfh.bti7535.w2017.kiwi.filter.Preprocessor;
 import weka.attributeSelection.InfoGainAttributeEval;
 import weka.attributeSelection.Ranker;
+import ch.bfh.bti7535.w2017.kiwi.baseline.SentiWordNetDemo;
 import weka.core.Instances;
 import weka.core.converters.TextDirectoryLoader;
 import weka.core.stemmers.SnowballStemmer;
@@ -23,6 +24,45 @@ import java.util.Map;
 public class App {
 
     public static void main(String[] args) throws Exception {
+        // BASELINE
+        SentiWordNetDemo classifier = new SentiWordNetDemo();
+
+        // negative Files
+        int countNegCorrect=0, countNegWrong=0;
+        File[] negFiles = new File(App.class.getClassLoader().getResource("txt_sentoken/neg").getPath()).listFiles();
+        for (File file: negFiles) {
+            classifier.load(App.class.getClassLoader()
+                    .getResource("txt_sentoken/neg/" + file.getName()).getPath());
+            if( classifier.classifyAllPOSN() == "no") countNegCorrect++;
+            else countNegWrong++;
+        }
+        System.out.println("countNegCorrect: " + countNegCorrect);
+        System.out.println("countNegWrong: " + countNegWrong);
+        System.out.println();
+
+        // positive Files
+        int countPosCorrect=0, countPosWrong=0;
+        File[] posFiles = new File(App.class.getClassLoader().getResource("txt_sentoken/pos").getPath()).listFiles();
+        for (File file: posFiles) {
+            classifier.load(App.class.getClassLoader()
+                    .getResource("txt_sentoken/pos/" + file.getName()).getPath());
+            if( classifier.classifyAllPOSN() == "yes") countPosCorrect++;
+            else countPosWrong++;
+        }
+        System.out.println("countPosCorrect: " + countPosCorrect);
+        System.out.println("countPosWrong: " + countPosWrong);
+        System.out.println();
+
+        System.out.println("Correct: " + (double) Math.round( (countPosCorrect+countNegCorrect) / 2000.0 * 100) / 100);
+        System.out.println("Error: " + (double) Math.round( (countPosWrong+countNegWrong) / 2000.0 * 100) / 100);
+        System.out.println();
+        // CLASSIFIER       POS_CORRECT POS_WRONG   NEG_CORRECT NEG_WRONG   CORRECT ERROR
+        // classifyAllPOSY  899         101         221         779         0.56    0.44
+        // classifyAllPOSN  892         108         231         769         0.56    0.44
+        // classifyADJY     856         144         341         659         0.6     0.4
+        // classifyADJN     843         157         369         631         0.61    0.39
+        // ***************************************************
+
         TextDirectoryLoader loader = new TextDirectoryLoader();
         loader.setDirectory(new File(App.class.getClassLoader()
                                               .getResource("txt_sentoken")
